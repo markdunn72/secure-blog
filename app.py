bdb20ce768429284c396615f73d120b5061cdff2
MARKDUNN72 <markdunn2012@gmail.com>
2018-11-15 22:41:48 +0000

    adapted previous site for added security, still need to add new_post, email_reset and password_reset

---
+++ app.py
@@ -0,0 +1,332 @@
+from flask import (
+    Flask, request, render_template,
+    redirect, url_for, flash, session, g)
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+import hashlib
+import sqlite3
+import os
+from functools import wraps
+import datetime
+
+app = Flask(__name__)
+
+
+# ADD ESCAPING FOR USER INPUT
+def escape(user_input):
+    return user_input
+
+
+# decorators
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def std_context(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        context = {}
+        request.context = context
+        if 'username' in session:
+            context['loggedin'] = True
+            context['username'] = session['username']
+        else:
+            context['loggedin'] = False
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+# # #
+# INDEX
+#
+@app.route('/')
+@std_context
+def index():
+    posts = get_index_posts()
+
+    def fix(item):
+        item['DATE'] = datetime.datetime.fromtimestamp(item['DATE']).strftime('%Y-%m-%d %H:%M')
+        item['CONTENT'] = '%s...' % (item['CONTENT'][:200])
+        return item
+
+    context = request.context
+    if posts:
+        context['posts'] = map(fix, posts)
+    else:
+        context['posts'] = posts
+
+    return render_template('index.html', context=context)
+
+
+@app.route('/search/')
+@std_context
+def search_page():
+    context = request.context
+    search_query = request.args.get('s', '')
+    posts = search_posts(search_query)
+
+    for post in posts:
+        post['content'] = '%s...'%(post['content'][:50])
+    context['posts'] = posts
+    context['query'] = search_query
+    return render_template('search_results.html', context=context)
+
+
+# # #
+# USER ACCOUNTS
+#
+default_account_error = u'There was an error with the account credentials'
+
+
+# user forms
+class RegistrationForm(Form):
+    username = StringField('Username', [
+        validators.DataRequired(),
+    ])
+    email = StringField('Email Address', [
+        validators.Email(),
+    ])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    registration_error = 'Unable to register account using these credentials'
+
+    # override validation to ensure username and email are unique
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+
+    def validate(self):
+        rv = Form.validate(self)
+        if not rv:
+            return False
+        if not check_if_username_exists(self.username.data) and check_if_email_exists(self.email.data):
+            # use same error message in same location with random delay - ACCOUNT ENUMERATION PROTECTION
+            enumeration_delay()
+            self.confirm.errors.append(self.registration_error)
+            return False
+
+        return True
+
+
+class LoginForm(Form):
+    username = StringField('Username', [
+        validators.DataRequired('Please enter your username')
+    ])
+    password = PasswordField('Password', [
+        validators.DataRequired('Please enter your password')
+    ])
+    login_error = 'Unable to login using these credentials'
+
+    # override validation to include credentials check
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+
+    def validate(self):
+        rv = Form.validate(self)
+        if not rv:
+            return False
+        if not validate_credentials(self.username.data, self.password.data):
+            # use same error message in same location with random delay - ACCOUNT ENUMERATION PROTECTION
+            enumeration_delay()
+            self.password.errors.append(self.login_error)
+            return False
+
+        return True
+
+
+# used to stop account enumeration using retrieval time to make guesses
+def enumeration_delay():
+    from time import sleep
+    import random
+    # sleep from 0.2 to 0.6 seconds
+    sleep(random.uniform(0.2, 0.6))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+@std_context
+def register():
+    context = request.context
+    if context['loggedin']:
+        return redirect(url_for('index'))
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+
+        # hash password for storage
+        hashed_password = hashlib.sha3_512(str.encode(password))
+        add_user_to_database(username, hashed_password.hexdigest(), email)
+        session['username'] = username
+        return redirect(url_for('login'))
+
+    return render_template('registration.html', form=form, context=context)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+@std_context
+def login():
+    context = request.context
+    if context['loggedin']:
+        return redirect(url_for('index'))
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        session['username'] = form.username.data
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form, context=context)
+
+
+@app.route('/logout')
+@std_context
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/<uname>/')
+@std_context
+def users_posts(uname=None):
+    if not check_if_username_exists(uname):
+        return 'RETURN MOCK USER_POSTS PAGE WITH FALSE UNAME'
+
+    def fix(item):
+        item['DATE'] = datetime.datetime.fromtimestamp(item['DATE']).strftime('%Y-%m-%d %H:%M')
+        return item
+
+    context = request.context
+    posts = get_user_posts(get_userid(uname))
+
+    if posts:
+        context['posts'] = map(fix, posts)
+    else:
+        context['posts'] = posts
+
+    return render_template('user_posts.html', context=context)
+
+
+# # #
+# DATABASE FUNCTIONS
+#
+# secured by using parameterized statements and appcontext teardown of db
+#
+DATABASE = 'blogsite.sqlite'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+
+    def make_dicts(cursor, row):
+        return dict((cursor.description[idx][0], value)
+                    for idx, value in enumerate(row))
+
+    db.row_factory = make_dicts
+
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def validate_credentials(username, password):
+    connection = get_db()
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM USERS WHERE USERNAME = ?", (username,))
+        rows = cursor.fetchall()
+        if not rows:
+            return rows
+        stored_password = rows[0]['PASSWORD']
+
+        # we're comparing the hashes rather than passwords themselves - we don't actually store the password
+        hash_of_entered_password = hashlib.sha3_512(str.encode(password)).hexdigest()
+
+        return stored_password == hash_of_entered_password
+
+
+def check_if_username_exists(username):
+    connection = get_db()
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM USERS")
+        rows = cursor.fetchall()
+    return any(username in user_dict['USERNAME'] for user_dict in rows)
+
+
+def check_if_email_exists(email):
+    connection = get_db()
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM USERS")
+        rows = cursor.fetchall()
+    return any(email in user_dict['EMAIL'] for user_dict in rows)
+
+
+def add_user_to_database(username, password, email):
+    connection = get_db()
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT MAX(USERID) FROM USERS")
+        userid = (cursor.fetchone()['MAX(USERID)']) + 1
+        cursor.execute("INSERT INTO USERS VALUES (?, ?, ?, ?)", (userid, username, password, email))
+
+
+def get_userid(username):
+    connection = get_db()
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT USERID FROM USERS WHERE USERNAME = ?", (username,))
+        userid = cursor.fetchone()['USERID']
+    return userid
+
+
+def get_user_posts(userid):
+    connection = get_db()
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT DATE,TITLE,CONTENT FROM POSTS WHERE CREATOR = ? ORDER BY DATE DESC", (userid,))
+        rows = cursor.fetchall()
+    return rows if rows else None
+
+
+def get_index_posts():
+    connection = get_db()
+    with connection:
+        cursor = connection.cursor()
+        statement = '''SELECT POSTS.CREATOR,POSTS.DATE,POSTS.TITLE,POSTS.CONTENT,USERS.USERNAME 
+        FROM POSTS JOIN USERS ON POSTS.CREATOR=USERS.USERID ORDER BY DATE DESC LIMIT 10'''
+        cursor.execute(statement)
+        rows = cursor.fetchall()
+    return rows if rows else None
+
+
+def search_posts(query):
+    connection = get_db()
+    with connection:
+        cursor = connection.cursor()
+        statement = '''SELECT POSTS.CREATOR,POSTS.TITLE,POSTS.CONTENT,USERS.USERNAME FROM POSTS JOIN USERS 
+        ON POSTS.CREATOR=USERS.USERID WHERE TITLE LIKE '%%?%%' ORDER BY DATE DESC LIMIT 10;'''
+        cursor.execute(statement, (query,))
+        rows = cursor.fetchall()
+    return rows if rows else None
+
+
+if __name__ == '__main__':
+    # secure session key
+    app.secret_key = os.urandom(16)
+    # set debug=False for production
+    app.run(debug=True)
---
+++ create_db.py
@@ -0,0 +1,112 @@
+import datetime
+import os
+import sqlite3
+import string
+import random
+import lipsum
+import re
+
+DATABASE = 'blogsite.sqlite'
+
+# From http://listofrandomnames.com/index.cfm?textarea
+USERS=map(lambda x:x.strip(), re.split('[\r\n]+','''Aleida King  
+Billye Quayle  
+Mildred Beaty  
+Adeline Beyers  
+Tricia Wendel  
+Kizzy Bedoya  
+Marx Warn  
+Hulda Culberson  
+Devona Morvant  
+Winston Tomasello  
+Dede Frame  
+Lissa Follansbee  
+Timmy Dapolito  
+Gracie Lonon  
+Nana Officer  
+Yuri Kruchten  
+Chante Brasch  
+Edmond Toombs  
+Scott Schwan  
+Lean Beauregard  
+Norberto Petersen  
+Carole Costigan  
+Chantel Drumheller  
+Riva Redfield  
+Jennie Sandifer  
+Vivian Cimini  
+Goldie Hayworth  
+Tomeka Kimler  
+Micaela Juan  
+Jerrold Tjaden  
+Collene Olson  
+Edna Serna  
+Cleveland Miley  
+Ena Haecker  
+Huey Voelker  
+Annamae Basco  
+Florentina Quinlan  
+Eryn Chae  
+Mozella Mcknight  
+Ruby Cobble  
+Jeannine Simerly  
+Colby Tabares  
+Jason Castorena  
+Asia Mosteller  
+Betsy Mendelsohn  
+Nicolle Leverette  
+Bobette Tuel  
+Lizabeth Borchert  
+Danica Halverson  
+Consuelo Crown'''))
+
+def create():
+    db = sqlite3.connect(DATABASE)
+
+    c = db.cursor()
+
+    c.execute(
+        'CREATE TABLE USERS'
+        '(USERID INTEGER PRIMARY KEY, USERNAME TEXT NOT NULL UNIQUE, '
+        'PASSWORD TEXT NOT NULL, EMAIL TEXT NOT NULL UNIQUE);')
+    c.execute(
+        'CREATE TABLE POSTS'
+        '(POSTID INTEGER PRIMARY KEY, CREATOR INTEGER NOT NULL REFERENCES USERS(USERID), '
+        'DATE INTEGER, TITLE TEXT NOT NULL, CONTENT TEXT NOT NULL)')
+    c.execute('''CREATE INDEX USER_USERNAME on USERS (USERNAME)''')
+    c.execute('''CREATE INDEX USER_POSTS on POSTS (CREATOR,DATE)''')
+    db.commit()
+
+    id = 0
+    for user in USERS:
+        create_content(db, id, user)
+        id += 1
+    db.commit()
+
+
+def create_content(db, id, name):
+    password = 'password'
+    c = db.cursor()
+    username = '%s%s' % (name.lower()[0], name.lower()[name.index(' ') + 1:])
+    email = '%s.%s@email.com' % ((name.lower()[0], name.lower()[name.index(' ') + 1:]))
+    c.execute('INSERT INTO USERS (userid, username, password, email) VALUES (?,?,?,?)',
+              (id, username, password, email))
+    date = datetime.datetime.now() - datetime.timedelta(28)
+
+    for i in range(random.randrange(4, 8)):
+        content = lipsum.generate_paragraphs(random.randrange(2, 6))
+        title = lipsum.generate_words(8)
+        date = date + datetime.timedelta(random.randrange(1, 3), minutes=random.randrange(1, 120),
+                                         hours=random.randrange(0, 6))
+
+        c.execute('INSERT INTO posts (creator,date,title,content) VALUES (?,?,?,?)',
+                  (id, date.timestamp(), title, content))
+
+def delete_db():
+    if os.path.exists(DATABASE):
+        os.remove(DATABASE)
+
+
+if __name__ == '__main__':
+    delete_db()
+    create()
